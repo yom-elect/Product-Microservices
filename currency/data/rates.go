04b033eebf40ec10bfff4adaefe261a6f4847d3c
Oriyomi adepitan <adepitanoriyomi11@yahoo.com>
2022-02-06 23:03:08 +0000

    refactor updates

--- currency/data/rates.go
+++ currency/data/rates.go
@@ -9,20 +9,34 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
-type ExchangeRate struct {
+type ExchangeRates struct {
 	log hclog.Logger
 	rates map[string]float64
 }
 
-func NewRates(l hclog.Logger) (*ExchangeRate, error) {
-	er := &ExchangeRate{log: l, rates: map[string]float64{}}
+func NewRates(l hclog.Logger) (*ExchangeRates, error) {
+	er := &ExchangeRates{log: l, rates: map[string]float64{}}
 
-	err := er.getRates()
+	err := er.GetRates()
 
 	return er, err
 }
 
-func (e *ExchangeRate) getRates() error {
+func (e *ExchangeRates) GetRate(base string, dest string) (float64, error) {
+	br, ok := e.rates[base]
+	if !ok {
+		return 0,fmt.Errorf("rate not found for currency %s", base)
+	}
+
+	dr, ok := e.rates[dest]
+	if !ok {
+		return 0,fmt.Errorf("rate not found for currency %s", dest)
+	}
+
+	return dr / br , nil
+}
+
+func (e *ExchangeRates) GetRates() error {
 	resp, err :=  http.DefaultClient.Get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
 	if err != nil {
 		return nil
@@ -45,6 +59,8 @@ func (e *ExchangeRate) getRates() error {
 
 		e.rates[c.Currency] = r
 	}
+
+	e.rates["EUR"] = 1
 
 	return nil
 }
